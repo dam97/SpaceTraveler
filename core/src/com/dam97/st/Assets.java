7bc4d9b95cccb3129a0e383ed168c7d4fc5bd44f
Humingrid <indienezja@gmail.com>
2016-02-03 20:43:31 +0100

    Create class for managing all game assets

---
+++ core/src/com/dam97/st/Assets.java
@@ -0,0 +1,14 @@
+package com.dam97.st;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+/**
+ * Created by dam97 on 2016-02-03.
+ */
+public class Assets {
+    public static final AssetManager assetManager = new AssetManager();
+
+    public static void load(){
+
+    }
+}
