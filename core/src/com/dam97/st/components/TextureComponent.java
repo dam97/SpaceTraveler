9ae5d887cd6ce4eb643414c576b42079dbeb7281
Humingrid <indienezja@gmail.com>
2016-02-07 02:34:08 +0100

    Create component for storing texture

---
+++ core/src/com/dam97/st/components/TextureComponent.java
@@ -0,0 +1,29 @@
+package com.dam97.st.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Pool;
+
+/**
+ * Created by dam97 on 2016-02-07.
+ */
+public class TextureComponent implements Component ,Pool.Poolable {
+    public TextureRegion textureRegion;
+
+    public TextureComponent(TextureRegion textureRegion) {
+        this.textureRegion = textureRegion;
+    }
+
+    public TextureRegion getTextureRegion() {
+        return textureRegion;
+    }
+
+    public void setTextureRegion(TextureRegion textureRegion) {
+        this.textureRegion = textureRegion;
+    }
+
+    @Override
+    public void reset() {
+        textureRegion = new TextureRegion();
+    }
+}
