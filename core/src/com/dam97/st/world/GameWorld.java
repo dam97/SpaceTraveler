fb9c48aa645fe9857dfdf1d14832eeba7467118b
Humingrid <indienezja@gmail.com>
2016-02-07 16:05:53 +0100

    Create world for all game actions, events and entities

---
+++ core/src/com/dam97/st/world/GameWorld.java
@@ -0,0 +1,14 @@
+package com.dam97.st.world;
+
+import com.badlogic.ashley.core.PooledEngine;
+
+/**
+ * Created by dam97 on 2016-02-07.
+ */
+public class GameWorld {
+    private PooledEngine engine;
+
+    public GameWorld(PooledEngine engine) {
+        this.engine = engine;
+    }
+}
