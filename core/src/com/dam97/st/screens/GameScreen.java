083a247bbb86ebfe39ddece3d1ce0cc33462acd5
Humingrid <indienezja@gmail.com>
2016-02-07 21:56:15 +0100

    Add entity world and engine

--- core/src/com/dam97/st/screens/GameScreen.java
+++ core/src/com/dam97/st/screens/GameScreen.java
@@ -1,22 +1,29 @@
 package com.dam97.st.screens;
 
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.dam97.st.SpaceTravelerGame;
+import com.dam97.st.world.GameWorld;
 
 /**
  * Created by dam97 on 2016-02-03.
  */
 public class GameScreen extends ScreenAdapter{
     SpaceTravelerGame game;
+    GameWorld gameWorld;
+    PooledEngine engine;
 
     public GameScreen(SpaceTravelerGame game) {
         super();
         this.game = game;
+        engine = new PooledEngine();
+        gameWorld = new GameWorld(engine);
     }
 
     @Override
     public void render(float delta) {
+        engine.update(delta);
         super.render(delta);
     }
 
