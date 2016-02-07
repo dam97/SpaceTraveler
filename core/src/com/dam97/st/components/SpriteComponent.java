c80145787ba2de784168655a839d148a38e996f0
Humingrid <indienezja@gmail.com>
2016-02-07 02:33:53 +0100

    Create component for storing sprite

---
+++ core/src/com/dam97/st/components/SpriteComponent.java
@@ -0,0 +1,25 @@
+package com.dam97.st.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.Pool;
+
+/**
+ * Created by dam97 on 2016-02-03.
+ */
+public class SpriteComponent implements Component, Pool.Poolable {
+    public Sprite sprite;
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    @Override
+    public void reset() {
+        sprite = new Sprite();
+    }
+}
