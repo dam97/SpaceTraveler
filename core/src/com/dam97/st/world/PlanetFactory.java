9db9512e84a38acd062c4b7d537131443371555a
Humingrid <indienezja@gmail.com>
2016-02-13 01:48:05 +0100

    Add UUIDComponent in creating new Planet

--- core/src/com/dam97/st/world/PlanetFactory.java
+++ core/src/com/dam97/st/world/PlanetFactory.java
@@ -3,6 +3,7 @@ package com.dam97.st.world;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
 import com.dam97.st.components.PlanetComponent;
+import com.dam97.st.components.UUIDComponent;
 
 /**
  * Created by dam97 on 2016-02-07.
@@ -17,9 +18,12 @@ public class PlanetFactory {
     public Entity createPlanet(String name, float size){
         Entity planet = engine.createEntity();
         PlanetComponent planetComponent = engine.createComponent(PlanetComponent.class);
+        // Unique ID for planet
+        UUIDComponent uuidComponent = engine.createComponent(UUIDComponent.class);
         planetComponent.setName(name);
         planetComponent.setSize(size);
         planet.add(planetComponent);
+        planet.add(uuidComponent);
         return planet;
     }
 }
