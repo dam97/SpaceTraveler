07aeed2caf9cf216a75f529583472277320277de
Humingrid <indienezja@gmail.com>
2016-02-28 02:18:21 +0100

    Change starting screen to menu

--- core/src/com/dam97/st/SpaceTravelerGame.java
+++ core/src/com/dam97/st/SpaceTravelerGame.java
@@ -5,22 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dam97.st.screens.GameScreen;
+import com.dam97.st.screens.MenuScreen;
 
 public class SpaceTravelerGame extends Game {
-	GameScreen gameScreen;
+	MenuScreen menuScreen;
 	SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		Assets.load();
 		batch = new SpriteBatch();
-		gameScreen = new GameScreen(this);
-		setScreen(gameScreen);
+		menuScreen = new MenuScreen(this);
+		setScreen(menuScreen);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		super.render();
 	}
