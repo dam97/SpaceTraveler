c247152979500245f5c3710352a101ae7a55d797
Humingrid <indienezja@gmail.com>
2016-03-10 12:23:26 +0100

    Create object serializers

---
+++ core/src/com/dam97/st/utils/serializers/ArraySerializer.java
@@ -0,0 +1,7 @@
+package com.dam97.st.utils.serializers;
+
+/**
+ * Created by dam97 on 2016-03-04.
+ */
+public class ArraySerializer {
+}
---
+++ core/src/com/dam97/st/utils/serializers/ImmutableArraySerializer.java
@@ -0,0 +1,7 @@
+package com.dam97.st.utils.serializers;
+
+/**
+ * Created by dam97 on 2016-03-03.
+ */
+public class ImmutableArraySerializer {
+}
